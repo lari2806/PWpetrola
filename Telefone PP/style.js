e9bd43b15541de05d2ace53be29b7797c2ff995a
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-03-04 18:32:44 -0300

    Add files via upload

---
+++ Telefone PP/style.js
@@ -0,0 +1,49 @@
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+const btn10 = document.querySelector("#btn10");
+const btn11 = document.querySelector("#btn11");
+const btn12 = document.querySelector("#btn12");
+const tel = document.querySelector("#tel");
+btn1.addEventListener("click", function() {
+    tel.value += btn1.value;
+});
+btn2.addEventListener("click", function() {
+    tel.value += btn2.value;
+});
+btn3.addEventListener("click", function() {
+    tel.value += btn3.value;
+});
+btn4.addEventListener("click", function() {
+    tel.value += btn4.value;
+});
+btn5.addEventListener("click", function() {
+    tel.value += btn5.value;
+});
+btn6.addEventListener("click", function() {
+    tel.value += btn6.value;
+})
+;btn7.addEventListener("click", function() {
+    tel.value += btn7.value;
+});
+btn8.addEventListener("click", function() {
+    tel.value += btn8.value;
+})
+;btn9.addEventListener("click", function() {
+    tel.value += btn9.value;
+});
+btn10.addEventListener("click", function() {
+    tel.value += btn10.value;
+});
+btn11.addEventListener("click", function() {
+    tel.value += btn11.value;
+});
+btn12.addEventListener("click", function() {
+    tel.value += btn12.value;
+});
