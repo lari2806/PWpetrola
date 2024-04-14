3026343e074a82444d500b2babdd2cea386ba25b
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-04-14 14:07:47 -0300

    Add files via upload

---
+++ Calculadora PP/Calc.js
@@ -0,0 +1,54 @@
+
+btn1.addEventListener("click", function() {
+    result.value += btn1.value;
+});
+btn2.addEventListener("click", function() {
+    result.value += btn2.value;
+});
+btn3.addEventListener("click", function() {
+    result.value += btn3.value;
+});
+btn4.addEventListener("click", function() {
+    result.value += btn4.value;
+});
+btn5.addEventListener("click", function() {
+    result.value += btn5.value;
+});
+btn6.addEventListener("click", function() {
+    result.value += btn6.value;
+})
+;btn7.addEventListener("click", function() {
+    result.value += btn7.value;
+});
+btn8.addEventListener("click", function() {
+    result.value += btn8.value;
+})
+;btn9.addEventListener("click", function() {
+    result.value += btn9.value;
+});
+btn10.addEventListener("click", function() {
+    result.value += btn10.value;
+});
+btn11.addEventListener("click", function() {
+    result.value += btn11.value;
+});
+btn12.addEventListener("click", function() {
+    result.value += btn12.value;
+});
+btn13.addEventListener("click", function() {
+    result.value += btn13.value;
+});
+btn14.addEventListener("click", function() {
+    result.value += btn14.value;
+});
+btn16.addEventListener("click", function() {
+    result.value += btn16.value;
+});
+clear.addEventListener("click", function() {
+    result.value = "";
+});
+btn15.addEventListener("click", function() {
+    result.value = eval(result.value);
+});
+
+
