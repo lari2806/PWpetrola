2bc4897f100175fa941f48e8cf62b80f5efaf9cb
Larissa Freitas <124395564+lari2806@users.noreply.github.com>
2025-04-26 12:20:15 -0300

    Corrigindo nomeclatura de variáveis.

--- Lista/Questão1/Questão1.js
+++ Lista/Questão1/Questão1.js
@@ -1,7 +1,7 @@
-function sum(x,y){
-    console.log("O primeiro número é " + x);
-    console.log("O segundo número é " + y);
-    result = x+y;
-    console.log("A soma destes dois números é igual a " + result )
+function sum(numero1,numero2){
+    console.log("O primeiro número é " + numero1);
+    console.log("O segundo número é " + numero2);
+    resultado = numero1+numero2;
+    console.log("A soma destes dois números é igual a " + resultado )
 }
 sum(2,9);
